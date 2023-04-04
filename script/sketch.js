37df3d31ba6bbd7eb37657d437c2fee058c5d85e
Mykal Coleman <mycoleman.art@gmail.com>
2023-04-04 10:15:02 -0700

    Please help me, set up css link and trying to get any sort of js to work

--- script/sketch.js
+++ script/sketch.js
@@ -1,19 +1,3 @@
-var frogX=300;
-var frogY=525;
-var frogHeight=40;
-var truckX=[0,200,400,600];
-
-function preload(){   ///// note we have to load images in this preload and not
-                      ///// in setup, assets need to be preloaded
-                      ///// ...also not these are "function" not "void"
+console.log("Hello from sketch.js!");
 
 
-}
-function setup() {    // put setup code here
-  createCanvas(600,600);       /// this is like size()
-
-
-}
-function loaded(){
-
-}
