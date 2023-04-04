49010da5dfc3936c03b913de2103465e9a026861
Mykal Coleman <mycoleman.art@gmail.com>
2023-04-04 10:32:14 -0700

    Why are you the way that you are

--- script/sketch.js
+++ script/sketch.js
@@ -1,3 +1,8 @@
-console.log("Hello from sketch.js!");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-
+const img = new Image();
+img.src = 'img\Track Sketch.jpeg';
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+}
