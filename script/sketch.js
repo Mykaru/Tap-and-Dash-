666885ffaa9d2897dc21c979393b29b35cfa5eca
Mykal Coleman <mycoleman.art@gmail.com>
2023-04-04 11:45:49 -0700

    AAAAAAAAAAA

--- script/sketch.js
+++ script/sketch.js
@@ -4,7 +4,7 @@ let windowHeight = screen.height;
 let windowWidth = screen.width;
 
 function preload() { //put the image in project directory and make this a relative path
-  bgImage = loadImage('C:\Users\mykal\OneDrive\Desktop\Tap-And-Dash\img\track-sketch.jpg');
+  bgImage = loadImage('img\track-sketch.jpg');
 }
 
 function setup() { //needed to specify windowWidth and windowHeight
