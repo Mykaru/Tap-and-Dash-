2b45c7465bae4efb5dbfd86ac16f67d22d02cad2
Mykal Coleman <mycoleman.art@gmail.com>
2023-04-06 10:09:53 -0700

    Starting with setting up the running boolean for gameplay

--- script/sketch.js
+++ script/sketch.js
@@ -1,7 +1,17 @@
+let playerOne = false;
+let count1 = 0;
+
+let playerTwo = false;
+let count2 = 0;
 
 let bgImage;
 let windowHeight = screen.height;
 let windowWidth = screen.width;
+
+function windowResized() {7
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 
 function preload() { 
   bgImage = loadImage("img/track-sketch.jpg");
@@ -16,6 +26,16 @@ function draw() {
   
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function toggleBoolean() {
+  isTrue = !isTrue;
+  if (isTrue) {
+    count++; // add 1 to count each time isTrue becomes true
+  }
 }
+
+document.addEventListener("keydown", function(event) {
+  if (event.code === "Space") {
+    toggleBoolean();
+    console.log("playerOne: " + playerOne + ", count1: " + count1);
+  }
+});
