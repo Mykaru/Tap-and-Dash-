0660168ef0967396f3e6836265aee8afc418b062
Mykal Coleman <mycoleman.art@gmail.com>
2023-04-06 10:27:18 -0700

    Both player one and player two are set up

--- script/sketch.js
+++ script/sketch.js
@@ -4,6 +4,7 @@ let lastKeyPressed1 = null;
 
 let playerTwo = false;
 let count2 = 0;
+let lastKeyPressed2 = null;
 
 let bgImage;
 let windowHeight = screen.height;
@@ -27,7 +28,7 @@ function draw() {
   
 }
 
-function toggleBoolean() {
+function toggleBoolean1() {
   playerOne = !playerOne;
   if (playerOne) {
     count1++; // add 1 to count each time isTrue becomes true
@@ -36,11 +37,31 @@ function toggleBoolean() {
 
 document.addEventListener("keydown", function(event) {
   if (event.code === "ArrowLeft" && lastKeyPressed1 !== "ArrowLeft") {
-    toggleBoolean();
+    toggleBoolean1();
     lastKeyPressed1 = "ArrowLeft";
   } else if (event.code === "ArrowUp" && lastKeyPressed1 !== "ArrowUp") {
-    toggleBoolean();
+    toggleBoolean1();
     lastKeyPressed1 = "ArrowUp";
   }
   console.log("playerOne: " + playerOne + ", count1: " + count1);
+});
+
+
+
+function toggleBoolean2() {
+  playerTwo = !playerTwo;
+  if (playerTwo) {
+    count2++; // add 1 to count each time isTrue becomes true
+  }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.code === "ArrowRight" && lastKeyPressed2 !== "ArrowRight") {
+    toggleBoolean2();
+    lastKeyPressed2 = "ArrowRight";
+  } else if (event.code === "ArrowDown" && lastKeyPressed2 !== "ArrowDown") {
+    toggleBoolean2();
+    lastKeyPressed2 = "ArrowDown";
+  }
+  console.log("playerTwo: " + playerTwo + ", count2: " + count2);
 });
